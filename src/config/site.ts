ba8255b83bf7a7679490d072d459cbaff78e519b
Giovani Alves <giovani.alves121@gmail.com>
2024-02-15 05:16:36 -0300

    feat: add sitemap and site config file

---
+++ src/config/site.ts
@@ -0,0 +1,9 @@
+import { SiteConfig } from "_/types";
+
+export const siteConfig: SiteConfig = {
+  name: "BeUni",
+  description:
+    "Precisa de um fornecedor de brindes personalizados? A BeUni cria seus produtos, armazena e agenda o envio. Você só precisa fazer o seu pedido no site.",
+  url: "http://localhost:3000",
+  ogImage: "https://www.beuni.com.br/wp-content/uploads/2023/07/Layer-2.png",
+};
